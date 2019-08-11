3d9cc9384d60f0c56d1320800594863cc679be27
Rajani Sharma <rantasukriti@gmail.com>
2019-08-11 12:43:55 +0530

    Updates

---
+++ precache-manifest.78c5f9822c742ff30495563966997af2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21aea4b08a43709ee93ba1ce2ae4f39c",
+    "url": "/Video_Streaming/index.html"
+  },
+  {
+    "revision": "f69c96d234d8a76637b2",
+    "url": "/Video_Streaming/static/js/2.f2f6c8a0.chunk.js"
+  },
+  {
+    "revision": "223b317f3f005d768050",
+    "url": "/Video_Streaming/static/js/main.5dc11305.chunk.js"
+  },
+  {
+    "revision": "8a491d2a7d3fb92d9cd4",
+    "url": "/Video_Streaming/static/js/runtime~main.337328a3.js"
+  }
+]);
